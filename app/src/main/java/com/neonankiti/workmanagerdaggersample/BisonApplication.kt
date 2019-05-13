82711b3e358c697875db18a052aa2e8b6d3d9da2
neonankiti <yk.nanri@gmail.com>
2019-05-13 15:13:21 +0900

    add performance monitoring

--- app/src/main/java/com/neonankiti/workmanagerdaggersample/BisonApplication.kt
+++ app/src/main/java/com/neonankiti/workmanagerdaggersample/BisonApplication.kt
@@ -1,15 +1,29 @@
 package com.neonankiti.workmanagerdaggersample
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.provider.Settings
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.perf.FirebasePerformance
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BisonApplication : Application() {
 
+    @SuppressLint("SimpleDateFormat", "HardwareIds")
     override fun onCreate() {
         super.onCreate()
         val factory: AppWorkerFactory = DaggerBisonAppComponent.create().factory()
         WorkManager.initialize(this, Configuration.Builder().setWorkerFactory(factory).build())
+        FirebaseAnalytics.getInstance(this)
+            .setUserId(Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID))
+        val trace = FirebasePerformance.getInstance().newTrace("ApplicationExecuted")
+        trace.start()
+        trace.putAttribute("Class", "BisonApplication")
+        val format = SimpleDateFormat("yyyy/MM/dd hh:mm:ss")
+        trace.putAttribute("datetime", format.format(Date()))
+        trace.stop()
     }
-
 }
